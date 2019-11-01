0385fdd67b0d7d5c4c795963905c09e9662d9b1c
marium <marium@fomarkmedia.com>
2019-11-01 13:47:27 +0500

    changes in tabs

--- app/src/main/java/com/mariumjawedsheikh/switchlibrary/MainActivity.java
+++ app/src/main/java/com/mariumjawedsheikh/switchlibrary/MainActivity.java
@@ -9,7 +9,7 @@ import com.mariumjawedsheikh.switchlibrary.R;
 
 public class MainActivity extends AppCompatActivity {
 
-    MainActivityLibrarySwitch aSwitch;
+    MainActivityLibrarySwitch aSwitch, aSwitch1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,13 +18,25 @@ public class MainActivity extends AppCompatActivity {
 
         aSwitch = findViewById(R.id.switchLibrary);
         aSwitch.setHeight(40);
-        aSwitch.setWidth(180);
-        aSwitch.setText1("Male");
-        aSwitch.setText2("Female");
+        aSwitch.setWidth(90);
+        aSwitch.setText1("On");
+        aSwitch.setText2("Off");
         aSwitch.setBackgroundColor(getResources().getColor(R.color.panic_blue));
         aSwitch.setTabColor(getResources().getColor(R.color.query_grey));
         // set selectedTab after: backgroundColor and TabColor
         // for tab 1 selected
         aSwitch.selectedTab(1);
+
+
+        aSwitch1 = findViewById(R.id.switchLibrary1);
+        aSwitch1.setHeight(40);
+        aSwitch1.setWidth(180);
+        aSwitch1.setText1("Male");
+        aSwitch1.setText2("Female");
+        aSwitch1.setBackgroundColor(getResources().getColor(R.color.material_purple500));
+        aSwitch1.setTabColor(getResources().getColor(R.color.query_grey));
+        // set selectedTab after: backgroundColor and TabColor
+        // for tab 1 selected
+        aSwitch1.selectedTab(2);
     }
 }
